cfdd75826aa1fe0e71ea68e06f677171c6b60ebb
Dipl.-Ing. (FH) Michael Czybor <Michael.Czybor@Live.de>
2024-07-17 14:50:51 +0200

    FIX return type

--- socket.h
+++ socket.h
@@ -17,5 +17,5 @@ bool socket_bind_and_listen(socket_handle_t socket);
 socket_handle_t socket_accept_incomming_connection(socket_handle_t socket, time_t receive_timeout_us);
 bool socket_connect(socket_handle_t socket, const char* ip, unsigned short port);
 bool socket_send(socket_handle_t socket, const void* data, size_t size_bytes);
-size_t socket_receive(socket_handle_t socket, char* data, size_t buffer_size);
+ssize_t socket_receive(socket_handle_t socket, char* data, size_t buffer_size);
 void socket_close(socket_handle_t* socket);
